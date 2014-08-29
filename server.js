4f792094327d6992c762d75620b09c466b244000
cyril <cyril@dkeystone.com>
2014-08-28 23:38:45 -0700

    fixed http

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
-var app = require('express')();
+var app = require('express')()
+var http = require('http').Server(app)
+var port = Number(process.env.PORT || 5000)
 
 app.get('/', function(req, res){
-  res.sendfile('index.html');
-});
+  res.sendfile('index.html')
+})
+
+http.listen(port, function(){
+  console.log('listening on *:'+port)
+})
